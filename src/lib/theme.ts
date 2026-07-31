505d0e1775d12f429cf2f4afb2cc868e53078126
AlistairHeus <kartikjha13@gmail.com>
2026-08-01 01:09:09 +0530

    predo

--- src/lib/theme.ts
+++
@@ -1,6 +0,0 @@
-import { PROJECT_THEMES, type ProjectThemeKey } from '../constants/theme';
-
-/**
- * Returns label and surface colors for a featured project theme key.
- */
-export const getProjectTheme = (themeKey: ProjectThemeKey) => PROJECT_THEMES[themeKey];
